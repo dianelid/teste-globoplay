d3ad78a0c6a486db3153771b51cc8b1411645173
Lidiane Santos <diane_lid@hotmail.com>
2020-04-13 23:46:30 -0300

    Updates

---
+++ precache-manifest.3eaa63524b58ef3a64d69f665c06096a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18fb4bc84e28665a2db8c5643e0b669",
+    "url": "/teste-globoplay/index.html"
+  },
+  {
+    "revision": "bc82df80e903121ad7cf",
+    "url": "/teste-globoplay/static/css/main.c7446496.chunk.css"
+  },
+  {
+    "revision": "8cdf168f7faabcc206dd",
+    "url": "/teste-globoplay/static/js/2.0cc7c59b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teste-globoplay/static/js/2.0cc7c59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc82df80e903121ad7cf",
+    "url": "/teste-globoplay/static/js/main.030e4582.chunk.js"
+  },
+  {
+    "revision": "6797e8f1afa7a7d4010f",
+    "url": "/teste-globoplay/static/js/runtime-main.811c6199.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/teste-globoplay/static/media/logo.ee7cd8ed.svg"
+  }
+]);
